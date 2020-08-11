fe23a0ca66ea85e993cbaba02a7a95eea82bdc6d
Spenser Black <spenserblack01@gmail.com>
2020-08-11 15:08:00 -0400

    Print basic GitHub info

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
 use cli::app;
+use github_stats::Repo;
 
 fn main() {
     let matches = app().get_matches();
 
     let repo = matches.value_of(cli::REPO_OPTION_NAME).unwrap();
+    let repo_stats = {
+        let mut repo = repo.split('/');
+        let owner = repo.next().expect("No repo owner");
+        let repo = repo.next().expect("No repo name");
+        Repo::new(owner, repo).expect("Could not fetch remote repo data")
+    };
+    println!("{}:", repo);
+    println!("URL: {}", repo_stats.clone_url());
+    println!("stargazers: {}", repo_stats.stargazers_count());
+    println!("subscribers: {}", repo_stats.subscribers_count());
+    println!("forks: {}", repo_stats.forks_count());
+    println!("created: {}", repo_stats.created_at());
+    println!("updated: {}", repo_stats.updated_at());
+    println!("size: {}KB", repo_stats.size());
+    println!("fork: {}", repo_stats.fork());
 }
 
 mod cli;
