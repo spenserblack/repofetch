72d7d177db9051061f72bd2be9cca059c9979b9f
Spenser Black <spenserblack01@gmail.com>
2020-08-11 15:55:48 -0400

    Reorder placement of emojis

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,18 @@ fn main() {
         Repo::new(owner, repo).expect("Could not fetch remote repo data")
     };
     println!("{}:", repo);
-    println!("URL: {}", repo_stats.clone_url());
-    println!("stargazers: {}{}", repo_stats.stargazers_count(), emojis::STAR);
-    println!("subscribers: {}{}", repo_stats.subscribers_count(), emojis::WATCHER);
-    println!("forks: {}", repo_stats.forks_count());
-    println!("created: {}{}", repo_stats.created_at(), emojis::CREATED);
-    println!("updated: {}", repo_stats.updated_at());
-    println!("size: {}", {
+    println!("{emoji}URL: {stat}", stat=repo_stats.clone_url(), emoji="");
+    println!("{emoji}stargazers: {stat}", stat=repo_stats.stargazers_count(), emoji=emojis::STAR);
+    println!("{emoji}subscribers: {stat}", stat=repo_stats.subscribers_count(), emoji=emojis::WATCHER);
+    println!("{emoji}forks: {stat}", stat=repo_stats.forks_count(), emoji="");
+    println!("{emoji}created: {stat}", stat=repo_stats.created_at(), emoji=emojis::CREATED);
+    println!("{emoji}updated: {stat}", stat=repo_stats.updated_at(), emoji="");
+    println!("{emoji}size: {stat}", emoji="", stat={
         let size = repo_stats.size();
         let size = size * 1_000; // convert from KB to just B
         size.big_byte(2)
     });
-    println!("fork: {}", repo_stats.fork());
+    println!("{emoji}fork: {stat}", stat=repo_stats.fork(), emoji="");
 }
 
 mod cli;
