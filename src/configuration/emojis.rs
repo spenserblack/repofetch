228e8a6cbd04547f5025622489f2c72faec7cf45
Spenser Black <spenserblack01@gmail.com>
2020-08-19 09:37:59 -0400

    Test width of emojis

--- src/configuration/emojis.rs
+++ src/configuration/emojis.rs
@@ -1,19 +1,36 @@
 macro_rules! define_emoji {
-    ($name:ident, $value:literal) => {
+    ($name:ident, $value:literal, $test_name:ident) => {
         pub(crate) const $name: &str = $value;
+
+        #[cfg(test)]
+        #[test]
+        fn $test_name() {
+            use unicode_width::UnicodeWidthStr;
+
+            assert_eq!(UnicodeWidthStr::width($name), 2);
+        }
     }
+
 }
 
-define_emoji!{URL, "🌐"}
-define_emoji!{STAR, "⭐"}
-define_emoji!{WATCHER, "👀"}
-define_emoji!{FORK, "🔱"}
-define_emoji!{ISSUE, "❗"}
-define_emoji!{PULL_REQUEST, "🔀"}
-define_emoji!{CREATED, "🐣"}
-define_emoji!{UPDATED, "📤"}
-define_emoji!{SIZE, "💽"}
-define_emoji!{NOT_FORK, "🥄"}
-define_emoji!{HACKTOBERFEST, "🎃"}
+define_emoji!{URL, "🌐", url}
+define_emoji!{STAR, "⭐", star}
+define_emoji!{WATCHER, "👀", watcher}
+define_emoji!{FORK, "🔱", fork}
+define_emoji!{ISSUE, "❗", issue}
+define_emoji!{PULL_REQUEST, "🔀", pr}
+define_emoji!{CREATED, "🐣", created}
+define_emoji!{UPDATED, "📤", updated}
+define_emoji!{SIZE, "💽", size}
+define_emoji!{NOT_FORK, "🥄", spoon}
+define_emoji!{HACKTOBERFEST, "🎃", jack_lantern}
 
 pub(crate) const EMPTY: &str = "  ";
+
+#[cfg(test)]
+#[test]
+fn empty() {
+    use unicode_width::UnicodeWidthStr;
+
+    assert_eq!(UnicodeWidthStr::width(EMPTY), 2);
+}
