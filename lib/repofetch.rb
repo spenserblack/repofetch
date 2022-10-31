578e4cae823ceec29dfd6a1bda7242ad05cdfcda
Spenser Black <spenserblack01@gmail.com>
2022-10-31 12:42:21 -0400

    Add methods to register plugins

--- lib/repofetch.rb
+++ lib/repofetch.rb
@@ -2,8 +2,24 @@
 
 # Main class for repofetch
 class Repofetch
-  # Add 2 to a number
-  def self.add2(num)
-    2 + num
+  @plugins = []
+  # Registers a plugin.
+  #
+  # @param [Plugin] plugin The plugin to register
+  def self.register_plugin(plugin)
+    @plugins << plugin
   end
+
+  # Base class for plugins.
+  class Plugin
+    # Registers this plugin class for repofetch.
+    def self.register
+      Repofetch.register_plugin(self)
+    end
+  end
+
+  def self.clear_plugins
+    @plugins = []
+  end
+  private_class_method :clear_plugins
 end
--- spec/repofetch_spec.rb
+++ spec/repofetch_spec.rb
@@ -3,9 +3,27 @@
 require 'repofetch'
 
 RSpec.describe Repofetch do
-  describe '#add2' do
-    it 'adds 2 and 3 and returns 5' do
-      expect(described_class.add2(3)).to eq 5
+  describe '#register_plugin' do
+    after { described_class.send(:clear_plugins) }
+
+    context 'with empty mock_plugin' do
+      let(:mock_plugin) { Class.new(described_class::Plugin) }
+
+      it 'adds a plugin to the list of plugins' do
+        expect(described_class.register_plugin(mock_plugin)).to eq [mock_plugin]
+      end
+    end
+  end
+
+  describe Repofetch::Plugin, '#register' do
+    after { Repofetch.send(:clear_plugins) }
+
+    context 'with empty mock_plugin' do
+      let(:mock_plugin) { Class.new(described_class) }
+
+      it 'adds a plugin to the list of plugins' do
+        expect(mock_plugin.register).to eq [mock_plugin]
+      end
     end
   end
 end
