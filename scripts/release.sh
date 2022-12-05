095f7e63e9e2f290aac07ddc15983166720d453d
Spenser Black <spenserblack01@gmail.com>
2022-12-05 16:05:55 -0500

    Set up release notes
    Uses a script to generate a tag from the release notes.

---
+++ scripts/release.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Generates a release tag from RELEASE_NOTES, temporarily setting
+# comments to ; to permit Markdown, creates the tag, then
+# clears RELEASE_NOTES and resets git comments.
+set -e
+
+TAG_NAME=$1
+TAG_TARGET=$2
+
+if git config core.commentChar; then
+	HAD_COMMENT_CHAR=1
+	COMMENT_CHAR=$(git config --get core.commentChar)
+else
+	HAD_COMMENT_CHAR=0
+fi
+
+git config core.commentChar ';'
+git tag -F RELEASE_NOTES $TAG_NAME $TAG_TARGET
+
+# Revert to original state
+if [ $HAD_COMMENT_CHAR -eq 1 ]; then
+	git config core.commentChar $COMMENT_CHAR
+else
+	git config --unset core.commentChar
+fi
+
+echo > RELEASE_NOTES
+git add RELEASE_NOTES
+git commit -m "Reset RELEASE_NOTES"
