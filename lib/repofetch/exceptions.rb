c8bad9d57480a8beea700c21e11049855b04a7e7
Spenser Black <spenserblack01@gmail.com>
2023-01-04 14:46:45 +0000

    Fix exception docs

--- lib/repofetch/exceptions.rb
+++ lib/repofetch/exceptions.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
-# Raised when there aren't any available plugins.
 class Repofetch
   class Error < RuntimeError
   end
 
+  # Raised when there aren't any available plugins.
   class NoPluginsError < Error
   end
 
+  # Raised when more than one plugin is activated.
   class TooManyPluginsError < Error
   end
 end
