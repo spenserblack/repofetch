8bc0a4a972c87a723c4d7d8c910c8da2583bdf6b
Spenser Black <spenserblack01@gmail.com>
2023-01-15 18:47:48 +0000

    Test config load methods

---
+++ spec/repofetch/config_spec.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require 'repofetch/config'
+
+RSpec.describe Repofetch::Config do
+  describe '#load' do
+    it 'calls #new with the contents of the given file' do
+      allow(File).to receive(:read).and_return('plugins: []')
+      described_class.load
+      expect(File).to have_received(:read).with(described_class.path)
+    end
+  end
+
+  describe '#load!' do
+    context 'when the file exists at the given path' do
+      before do
+        allow(File).to receive(:exist?).and_return(true)
+        allow(File).to receive(:read).and_return('plugins: []')
+        allow(File).to receive(:write)
+      end
+
+      it 'calls #new with the contents' do
+        described_class.load!
+        expect(File).to have_received(:read).with(described_class.path)
+      end
+
+      it 'does not write the default config' do
+        described_class.load!
+        expect(File).not_to have_received(:write)
+      end
+    end
+
+    context 'when the file does not exist at the given path' do
+      before do
+        allow(File).to receive(:exist?).and_return(false)
+        allow(File).to receive(:read).and_return('plugins: []')
+        allow(File).to receive(:write)
+      end
+
+      it 'writes the default config' do
+        described_class.load!
+        expect(File).to have_received(:write).with(described_class.path, described_class::DEFAULT_CONFIG)
+      end
+
+      it 'calls #new with the default config' do
+        allow(described_class).to receive(:new).and_call_original
+        described_class.load!
+        expect(described_class).to have_received(:new).with(described_class::DEFAULT_CONFIG)
+      end
+    end
+  end
+end
